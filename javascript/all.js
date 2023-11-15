39c83ef2eae870a069906992855ad88a8a740f92
nora2207 <nora2207@outlook.com>
2023-11-15 17:17:38 +0800

    first
    first

---
+++ javascript/all.js
@@ -0,0 +1,17 @@
+//typed
+const typed = new Typed('.typed', {
+  strings: ["My Name is Nora", "I'm a Front-end engineer", "This is my portfolio"],
+  typeSpeed: 100,
+  backDelay: 900,
+  // loop
+  loop: true
+});
+
+$(document).ready(function () {
+  //lightbox 效果
+  lightbox.option({
+      'resizeDuration': 500,
+      'wrapAround': true,
+    'showImageNumberLabel': false,
+  });
+});
